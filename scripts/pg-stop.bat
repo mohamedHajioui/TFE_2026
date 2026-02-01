a606f7c558a58484604199d79355032355049de2
Mohamed El Hajioui <mohamedelhajioui3@gmail.com>
2026-02-01 04:15:58 +0100

    installation des dependances et config de la base de donnée et automatisation avec scripts pg-setup.bat et pg-stop.bat

---
+++ scripts/pg-stop.bat
@@ -0,0 +1,30 @@
+@echo off
+
+REM Vérification des privilèges administrateur
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Ce script necessite des privileges administrateur.
+    echo Relancement avec elevation...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+echo ===========================================
+echo Arret de PostgreSQL...
+echo ===========================================
+
+REM Nom du service
+set SERVICE_NAME=postgresql-x64-18
+
+REM Arrête le service
+net stop %SERVICE_NAME%
+
+if %errorlevel%==0 (
+    echo ✅ PostgreSQL arrete avec succes !
+) else (
+    echo ❌ Erreur : impossible d'arreter PostgreSQL.
+    echo Verifie le nom du service dans le script.
+)
+
+echo ===========================================
+pause
