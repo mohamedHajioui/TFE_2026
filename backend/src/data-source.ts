fb0ec8d13f82c2edf1bf17b44a881ca33914f4c6
Mohamed El Hajioui <mohamedelhajioui3@gmail.com>
2026-02-01 22:51:39 +0100

    fix: correction de data-source pour la base de donnée

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,13 +1,16 @@
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '1234',
-  database: 'tfe_2026',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || '1234',
+  database: process.env.DB_DATABASE || 'tfe_2026',
   entities: ['src/**/*.entity.ts'],
   migrations: ['database/migrations/*.ts'],
-  migrationsTableName: 'migrations',
+  synchronize: false,
 });
